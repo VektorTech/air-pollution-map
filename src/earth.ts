14ef387e1c97eda9279d4b2b311a7e9e65e53679
VektorTech <kennymax18@gmail.com>
2022-12-10 02:11:14 -0500

    add x axis rotation logic

--- src/earth.ts
+++ src/earth.ts
@@ -17,7 +17,7 @@ export default class Earth {
   private rotationVelocity: Vector2;
   private rotationAcceleration: Vector2;
 
-  private readonly DAMP_FACTOR = 0.9;
+  private readonly DAMP_FACTOR = 0.88;
 
   constructor(scene: Scene) {
     const geometry = new SphereGeometry(1, 10, 10);
@@ -38,21 +38,33 @@ export default class Earth {
   }
 
   update(delta: number) {
+    delta = Math.min(delta, 0.033334);
+
     if (this.static) {
+      this.rotationAcceleration.x *= this.DAMP_FACTOR;
       this.rotationAcceleration.y *= this.DAMP_FACTOR;
+    } else {
+      this.rotationAcceleration.x = gsap.utils.interpolate(
+        this.rotationAcceleration.x,
+        -this.earth.rotation.x * delta,
+        0.1
+      );
+      this.rotationAcceleration.y = gsap.utils.interpolate(
+        this.rotationAcceleration.y,
+        Utils.degreesToRadians(5) * delta,
+        0.1
+      );
     }
 
-    this.rotationAcceleration.y = gsap.utils.interpolate(
-      this.rotationAcceleration.y,
-      Utils.degreesToRadians(5) * Math.min(delta, 0.033334),
-      0.1
-    );
-
+    this.rotationVelocity.x += this.rotationAcceleration.x;
     this.rotationVelocity.y += this.rotationAcceleration.y;
+
+    this.earth.rotation.x = this.rotationVelocity.x;
     this.earth.rotation.y = this.rotationVelocity.y;
   }
 
   onMoveInteraction(position: Vector2, movement: Vector2) {
+    this.rotationAcceleration.x += (movement.y / 20) * Number(this.static);
     this.rotationAcceleration.y += (movement.x / 10) * Number(this.static);
   }
 }

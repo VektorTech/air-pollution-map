e2128d6ccf9ccf93f1f53fa1cb82ab973fb61474
VektorTech <kennymax18@gmail.com>
2022-12-09 21:04:53 -0500

    add deg to rads util func

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 const Utils = Object.freeze({
   radiansToDegrees: (radians: number) => (radians * 180) / Math.PI,
+  degreesToRadians: (degrees: number) => (degrees * Math.PI) / 180,
 });
 
 export default Utils;

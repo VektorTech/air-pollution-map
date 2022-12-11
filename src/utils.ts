065de3fb6158c5e941322302683145c46e7fef6f
VektorTech <kennymax18@gmail.com>
2022-12-11 03:39:49 -0500

    add funcs to convert between spherical & cartesian coords

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,27 @@
+import { Vector3 } from "three";
+
 const Utils = Object.freeze({
   radiansToDegrees: (radians: number) => (radians * 180) / Math.PI,
   degreesToRadians: (degrees: number) => (degrees * Math.PI) / 180,
+  sphericalToCartesian: (
+    latitudeRadians: number,
+    longitudeRadians: number,
+    radius: number
+  ) =>
+    new Vector3(
+      Math.cos(latitudeRadians) * Math.cos(longitudeRadians) * radius,
+      Math.sin(latitudeRadians) * radius,
+      Math.cos(latitudeRadians) * Math.sin(longitudeRadians) * radius
+    ),
+  cartesianToDegrees: (vec: Vector3) => {
+    const { x, y, z } = vec;
+    const radius = vec.length();
+    const xz = Math.sqrt(x ** 2 + z ** 2);
+    const phi = Math.atan(y / xz);
+    const theta = Math.acos(x / xz);
+
+    return { radius, phi, theta };
+  },
 });
 
 export default Utils;

1eab2ab768450889ab21ccdf679d563277c8df3b
VektorTech <kennymax18@gmail.com>
2022-12-13 17:49:10 -0500

    use pointerdown/up to create a fast click event.
    - add functions to zoom in/out of active markers

--- src/earth.ts
+++ src/earth.ts
@@ -12,6 +12,9 @@ import {
   Intersection,
   Object3D,
   Event,
+  FrontSide,
+  BackSide,
+  Quaternion,
 } from "three";
 
 import Utils from "./utils";
@@ -23,7 +26,12 @@ import cloudsTextureShader from "./shaders/cloudsTexture.frag.glsl";
 export default class Earth {
   private earth: Mesh;
   private clouds: Mesh;
+
   private isPointerDown: boolean;
+  private pauseState: boolean;
+  private zoomState: boolean;
+  private activeMarker: Intersection;
+
   private clicked = false;
 
   private rotationVelocity: Vector2;
@@ -45,10 +53,10 @@ export default class Earth {
         "./assets/textures/cloudsDiffuse.jpg"
       );
 
-    const geometry = new SphereGeometry(1, 50, 50);
-    const cloudsGeometry = new SphereGeometry(1.01, 50, 50);
+    const geometry = new SphereGeometry(1, 60, 60);
+    const cloudsGeometry = new SphereGeometry(1.006, 60, 60);
 
-    const lightPos = new Vector3(-1.9, 1.5, -0.1);
+    const lightPos = new Vector3(-1.5, 1.8, -3.2);
     const material = new ShaderMaterial({
       vertexShader: standardVertexShader,
       fragmentShader: lightTextureShader,
@@ -65,6 +73,7 @@ export default class Earth {
         iCloudsAlphaMap: { value: cloudsTexture },
         iLightPos: { value: lightPos },
       },
+      side: FrontSide,
       transparent: true,
     });
 
@@ -76,15 +85,56 @@ export default class Earth {
 
     this.rotationAcceleration = new Vector2();
     this.rotationVelocity = new Vector2();
+
     this.isPointerDown = false;
+    this.pauseState = false;
+    this.zoomState = false;
 
     scene.add(this.earth);
 
-    addEventListener("pointerdown", () => (this.isPointerDown = true));
-    addEventListener("touchstart", () => (this.isPointerDown = true));
-    addEventListener("pointerup", () => (this.isPointerDown = false));
-    addEventListener("touchend", () => (this.isPointerDown = false));
-    addEventListener("click", () => (this.clicked = true));
+    const timeThreshold = 350;
+    const distanceThreshold = 5;
+
+    let pointerTime = 0;
+    let pointerPosX = 0;
+    let pointerPosY = 0;
+
+    addEventListener("pointerdown", ({ clientX, clientY }) => {
+      pointerTime = performance.now();
+      pointerPosX = clientX;
+      pointerPosY = clientY;
+      this.isPointerDown = true;
+    });
+    addEventListener("touchstart", ({ touches }) => {
+      if (touches.length == 1) {
+        pointerTime = performance.now();
+        pointerPosX = touches[0].clientX;
+        pointerPosY = touches[0].clientY;
+      }
+      this.isPointerDown = true;
+    });
+    addEventListener("pointerup", ({ clientX, clientY }) => {
+      const isClick =
+        new Vector2(clientX - pointerPosX, clientY - pointerPosY).length() <
+          distanceThreshold && performance.now() - pointerTime < timeThreshold;
+
+      this.clicked = isClick;
+      this.isPointerDown = false;
+    });
+    addEventListener("touchend", ({ changedTouches }) => {
+      const isClick =
+        new Vector2(
+          changedTouches[0].clientX - pointerPosX,
+          changedTouches[0].clientY - pointerPosY
+        ).length() < distanceThreshold &&
+        performance.now() - pointerTime < timeThreshold;
+
+      this.clicked = isClick;
+      this.isPointerDown = false;
+    });
+
+    // @ts-ignore
+    window.zoomOut = this.zoomOut.bind(this);
   }
 
   pinMarker(
@@ -93,11 +143,21 @@ export default class Earth {
     details?: Record<string, unknown>
   ) {
     const material = new MeshBasicMaterial({
-      color: 0xfefebe,
+      color: 0xffffff,
       transparent: true,
       opacity: 0.8,
     });
-    const sphere = new Mesh(new SphereGeometry(2e-2, 8, 8), material);
+    const sphere = new Mesh(
+      new SphereGeometry(0.05, 8, 8),
+      new MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        opacity: 0.0,
+        side: BackSide,
+      })
+    );
+    const sphereInner = new Mesh(new SphereGeometry(1.3e-2, 8, 8), material);
+    sphere.add(sphereInner);
     const latitudeRad = Utils.degreesToRadians(latitude);
     const longitudeRad = Utils.degreesToRadians(longitude);
 
@@ -120,32 +180,71 @@ export default class Earth {
   update(delta: number) {
     delta = Math.min(delta, 0.033334);
 
-    if (this.isPointerDown) {
-      this.rotationAcceleration.y *= this.DAMP_ROT_FACTOR_Y;
-    } else {
-      this.rotationAcceleration.x *= this.DAMP_ROT_FACTOR_X;
+    if (!this.zoomState) {
+      if (this.isPointerDown || this.pauseState) {
+        this.rotationAcceleration.y *= this.DAMP_ROT_FACTOR_Y;
+      } else {
+        this.rotationAcceleration.x *= this.DAMP_ROT_FACTOR_X;
 
-      this.rotationAcceleration.y = gsap.utils.interpolate(
-        this.rotationAcceleration.y,
-        Utils.degreesToRadians(5) * delta,
+        this.rotationAcceleration.y = gsap.utils.interpolate(
+          this.rotationAcceleration.y,
+          Utils.degreesToRadians(5) * delta,
+          0.1
+        );
+      }
+
+      this.earth.rotation.x = gsap.utils.interpolate(
+        this.earth.rotation.x,
+        this.rotationAcceleration.x,
         0.1
       );
+
+      this.rotationVelocity.y += this.rotationAcceleration.y;
+      this.earth.rotation.y = this.rotationVelocity.y;
     }
 
-    this.earth.rotation.x = gsap.utils.interpolate(
-      this.earth.rotation.x,
-      this.rotationAcceleration.x,
-      0.1
-    );
-
-    this.rotationVelocity.y += this.rotationAcceleration.y;
-    this.earth.rotation.y = this.rotationVelocity.y;
-
     this.clouds.rotation.y += Math.PI * delta * 3e-2;
+    this.clouds.rotation.z += -Math.PI * delta * 1e-2;
+  }
+
+  private zoomIn(markerIntersection: Intersection) {
+    if (!markerIntersection) return;
+
+    const destination = Utils.sphericalToCartesian(Math.PI / 2, 0);
+    const endQuaternion = new Quaternion().setFromUnitVectors(
+      markerIntersection.object.position.clone().normalize(),
+      destination.normalize()
+    );
+    const startQuaternion = this.earth.quaternion.clone().normalize();
+    this.zoomState = true;
+
+    let p = { step: 0 };
+    gsap
+      .to(p, { step: 1, duration: 1 })
+      .eventCallback("onUpdate", () => {
+        const inQuaternionRange = startQuaternion.slerp(endQuaternion, p.step);
+        this.earth.setRotationFromQuaternion(inQuaternionRange);
+      })
+      .eventCallback("onComplete", () => {
+        this.activeMarker = markerIntersection;
+      });
+  }
+
+  private zoomOut() {
+    this.earth.rotation.set(0, 0, 0);
+    this.zoomState = false;
+    this.activeMarker = null;
   }
 
   checkActiveObjects(intersections: Intersection<Object3D<Event>>[]) {
+    const markerIntersection = intersections.find(
+      (current) => current.object.name == "Marker"
+    );
+    this.pauseState = markerIntersection && markerIntersection.distance < 2.5;
+
     if (this.clicked) {
+      this.zoomIn(markerIntersection);
+
       const earthIntersection = intersections.find(
         (current) => current.object.name == this.name
       );
@@ -165,14 +264,16 @@ export default class Earth {
 
   onMoveInteraction(position: Vector2, movement: Vector2) {
     const limit = Utils.degreesToRadians(60);
-    this.rotationAcceleration.x += -movement.y * Number(this.isPointerDown);
-    this.rotationAcceleration.x = gsap.utils.clamp(
-      -limit,
-      limit,
-      this.rotationAcceleration.x
-    );
+    if (!this.zoomState) {
+      this.rotationAcceleration.x += -movement.y * Number(this.isPointerDown);
+      this.rotationAcceleration.x = gsap.utils.clamp(
+        -limit,
+        limit,
+        this.rotationAcceleration.x
+      );
 
-    this.rotationAcceleration.y +=
-      (movement.x / 10) * Number(this.isPointerDown);
+      this.rotationAcceleration.y +=
+        (movement.x / 10) * Number(this.isPointerDown);
+    }
   }
 }

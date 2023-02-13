e1dc2a846bd020826775e575fbb9e2589b2189ec
VektorTech <kennymax18@gmail.com>
2023-02-13 04:00:54 -0500

    refactor

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,19 @@
+const API_ADDRESS = "http://localhost:5500";
+
 export const getGlobalData = async () =>
-  fetch("http://localhost:5500/aqi-data").then((res) => res.json());
+  fetch(`${API_ADDRESS}/aqi-data`).then((res) => res.json());
 
 export const getIQAirData = async (lat: number, long: number) =>
-  fetch(`http://localhost:5500/aqi-data/iqair/geo:${lat};${long}`).then((res) =>
+  fetch(`${API_ADDRESS}/aqi-data/iqair/geo:${lat};${long}`).then((res) =>
     res.json()
   );
 
 export const getOpenWeatherMapData = async (lat: number, long: number) =>
   fetch(
-    `http://localhost:5500/aqi-data/openweathermap/geo:${lat};${long}`
+    `${API_ADDRESS}/aqi-data/openweathermap/geo:${lat};${long}`
   ).then((res) => res.json());
 
 export const getWAQIData = async (lat: number, long: number) =>
-  fetch(`http://localhost:5500/aqi-data/waqi/geo:${lat};${long}`).then((res) =>
+  fetch(`${API_ADDRESS}/aqi-data/waqi/geo:${lat};${long}`).then((res) =>
     res.json()
   );
